aa8450fcf0e869bee5028911500a26f7f8506dc0
devloperpalak <palakbasak8@gmail.com>
2025-03-09 21:48:50 +0600

    Fix tailwindcss issue

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 "use client"
 
 import Header from "@/Components/Header";
-import BlogItem from "@/Components/BlogItem";
-import Image from "next/image";
 import BlogList from "@/Components/BlogList";
 import Footer from "@/Components/Footer";
 import { ToastContainer } from "react-toastify";
